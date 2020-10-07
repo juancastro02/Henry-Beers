5fc05c085c7f1b50e0d12b3c8abc5eb64d9f2b27
Ceci-Caccamo <cecicaccamo@gmail.com>
2020-10-07 20:40:22 -0300

    S14 y S15 terminados y agregamos la nav con M UI

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,22 @@ import Product from './componentes/Product'
 import SearchBar from './componentes/searchBar'
 import CrudCurso from './Components/CrudCurso/CrudCurso'
 import CrudCategory from './Components/CrudCategory/CrudCategory'
+import { BrowserRouter, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar/NavBar'
+
+
 
 function App() {
   return (
     <div >
-      <SearchBar/>
-      <Product/>
-       <CrudCurso/>
-       <CrudCategory/>
+      <BrowserRouter>
+        <Route path='/' component={NavBar} />
+        <Route exact path="/products/:productoId" component={Product} />
+        <Route exact path="/category" component={CrudCategory} />
+        <Route exact path="/products/:productoId" component={Product} />
+        <Route exact path="/products" component={CrudCurso} />
+        {/* <Route exact path='/catalogo' component={Catalogo} /> */}
+      </BrowserRouter>
     </div>
   );
 }
