81bc256f92e690c508f28e1098fe1b5c2221ccfa
Ceci-Caccamo <cecicaccamo@gmail.com>
2020-10-07 12:09:15 -0300

    pusheado

---
+++ api/src/routes/category.js
@@ -0,0 +1,16 @@
+const server = require('express').Router();
+const { Category } = require('../db.js');
+
+server.post('/create', (req, res)=>{
+    const {name, description} = req.body
+    Category.findOrCreate({where:{name, description}})
+    .then(()=>{
+        res.status(201).send('Categoria agregada exitosamente')
+    })
+    .catch(err=>{
+        res.status(400).json(err)
+    })
+})
+ 
+
+module.exports = server
