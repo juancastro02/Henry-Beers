f1a8588a3982c186cab73eed5420a4f3687cb28c
jorgeelieser <giorgioramirez@gmail.com>
2020-10-09 17:53:06 -0500

    subiendo

--- client/src/Components/CrudBeer/CrudBeer.js
+++ client/src/Components/CrudBeer/CrudBeer.js
@@ -3,14 +3,19 @@ import axios from 'axios'
 import './CrudBeer.css'
 import {useSelector, useDispatch} from 'react-redux'
 import {getCategory} from '../../Redux/category'
+import {getbeers} from '../../Redux/beer'
 
 const CrudBeer = () => {
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch() 
   const categories = useSelector(store=> store.category.categories)
+  const beers = useSelector(store => store.beer.beers)
+
   useEffect(()=>{
    dispatch(getCategory())
+   dispatch(getbeers())
   },[])
+
 
    const [video, setVideo] = useState({
        name: "",
@@ -23,7 +28,29 @@ const CrudBeer = () => {
     
  const {name, description, price, stock, image, category} = video
 
-   const submitBeer= async()=>{
+   const UpdateBeer = async()=>{
+     
+    const id = handleGet(id)
+    const info = {
+      name: video.name,
+      description: video.description ,
+      price: video.price,
+      stock: video.stock,
+      image: video.image,
+      category: video.category
+    }
+
+    console.log(id)
+    const {data} = await axios.put(`http://localhost:4000/products/update/${id}`)
+
+   }
+
+   const handleGet =(id)=>{
+    return id 
+   }
+
+   const submitBeer= async(id)=>{
+    handleGet(id)
      const info = {
        name: video.name,
        description: video.description ,
@@ -69,8 +96,17 @@ const CrudBeer = () => {
             <span>{e.name}</span>
             </Fragment>
           ))}
-          <button type='submit' onClick={()=> submitBeer()} >Enviar</button> 
+          <button type='submit' onClick={()=> submitBeer()} >Add product</button> 
+          <button type='submit' onClick={()=> UpdateBeer()} >Update product</button> 
         </form>
+
+        <div>
+          {beers.map(e=>(
+            <button onClick={()=> handleGet(e.id)} >
+              {e.name} 
+            </button>
+          ))}
+        </div>
         </div>
     )
 }
